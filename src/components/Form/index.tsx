5f9e6d17ff15a1f8d37b9b4453df3b0680623b52
yangyao <873241789@qq.com>
2018-08-10 15:20:52 +0800

    优化 发送验证码btn 集中处理 发送验证码

--- src/components/Form/index.tsx
+++ src/components/Form/index.tsx
@@ -4,6 +4,8 @@
  * date: 2018/8/8
  */
 import * as React from 'react';
+import { Request } from '../../cose/Request';
+import URL from '../../constants/URL';
 
 export interface Form {
     /**
@@ -23,6 +25,11 @@ export interface Form {
      * @param values
      */
     onSubmit: (values) => void;
+    /**
+     * 发送验证码
+     * @param values
+     */
+    onSand?: (values) => void;
 }
 
 export interface FormItem {
@@ -30,18 +37,23 @@ export interface FormItem {
     type: string;
     name: string;
     placeholder?: string;
-    after?: React.ReactNode;
+    pattern?: string;
+    maxLength?: number;
 }
 
 type State = {
     values: object;
+    countDown: number;
 };
 
 export default class Index extends React.Component<Form, State> {
     constructor(props, content) {
         super(props, content);
+        this.handleSubmit = this.handleSubmit.bind(this);
+        this.handleSand = this.handleSand.bind(this);
         this.state = {
-            values: {}
+            values: {},
+            countDown: 0
         };
     }
 
@@ -52,7 +64,21 @@ export default class Index extends React.Component<Form, State> {
      */
     renderFormItem(config: Array<FormItem>) {
         return config.map((values, index) => {
-            const {label, type, name, placeholder, after, ...other} = values;
+            const {label, type, name, placeholder, ...other} = values;
+            // 倒计时
+            const {countDown} = this.state;
+            let after: any = null;
+            // 拦截 验证码表单
+            if (name === 'checkValue') {
+                after = (
+                    <button className="verificationButton"
+                            onClick={this.handleSand}
+                            style={{
+                                pointerEvents: countDown === 0 ? 'auto' : 'none',
+                            }}
+                    >{countDown === 0 ? '发送验证码' : countDown}</button>
+                );
+            }
             return (
                 <section key={index}>
                     <label>{`${label}:`}</label>
@@ -66,6 +92,43 @@ export default class Index extends React.Component<Form, State> {
                     {after}
                 </section>
             )
+        });
+    }
+
+    /**
+     * 发送手机号
+     * @param e
+     */
+    handleSand(e) {
+        e.preventDefault();
+        const value: any = this.state.values;
+        console.log('发送验证码', value);
+        if (!value || !value.identity) {
+            alert('请填写手机号');
+            return;
+        }
+        Request({
+            url: `${URL.check}?identity=${value.identity}`
+        }).then(res => {
+            console.log(res);
+            if (res.status === '3001') {
+                alert(res.describe);
+            }
+            // 成功开始验证码倒数定时器
+            if (res.success) {
+                this.setState({
+                    countDown: 60
+                });
+                let timer = setInterval(() => {
+                    this.setState(preFix => ({
+                        countDown: preFix.countDown - 1
+                    }));
+                    if (this.state.countDown === 0) {
+                        clearInterval(timer);
+                    }
+                    console.log(this.state.countDown);
+                }, 1000);
+            }
         });
     }
 
@@ -102,7 +165,7 @@ export default class Index extends React.Component<Form, State> {
             <aside className="floatBox">
                 <h1 className="logo">{`${title}上福数据`}</h1>
                 <p className="descr">上福数据 是金融圈最具影响力的 技术服务供应商</p>
-                <form onSubmit={this.handleSubmit.bind(this)}>
+                <form onSubmit={this.handleSubmit}>
                     {this.renderFormItem(fields)}
                     <p className="service">
                         {this.props.children}
