a74e2b1185ab866bfd563ea467f8d0a12cdf74ae
yangyao <873241789@qq.com>
2018-08-10 19:12:20 +0800

    新增根据状态，修改边框 背景颜色

--- src/components/Alert.tsx
+++ src/components/Alert.tsx
@@ -3,12 +3,13 @@
  * author: yangyao(873241789@qq.com)
  * date: 2018/8/10
  */
+import '../styles/alert.scss';
 import * as ReactDOM from "react-dom";
 import * as React from "react";
 import ReactSVG from 'react-svg';
-import '../styles/alert.scss';
+import { statusCode } from '../constants/zh-cn';
 
-export default function alert(message) {
+export default function alert(message: string | object) {
     let body = document.body;
     let alert: any = document.getElementById('alert');
     let showDom: any = document.createElement("div");
@@ -38,18 +39,41 @@ export default function alert(message) {
         close();
     }, 2000);
     ReactDOM.render(
+        renderMessage(close, message),
+        showDom
+    );
+};
+
+function renderMessage(close, message) {
+    // 默认错误
+    let svgPath = '../assets/svg/wran.svg';
+    let messageString = message;
+    let borderColor = '(255,229,143,1)';
+    let backColor = 'rgba(255,251,230,1)';
+    // 如果是对象
+    if (message instanceof Object) {
+        const {status, success} = message;
+        messageString = statusCode[status];
+        svgPath = success && '../assets/svg/success.svg' || '../assets/svg/error.svg';
+        borderColor = success && 'rgba(183,235,143,1)' || 'rgba(255,163,158,1)';
+        backColor = success && 'rgba(246,255,237,1)' || 'rgba(246,255,237,1)';
+    }
+    return (
         <section className="alert"
                  onClick={close}
+                 style={{
+                     border: `2px solid ${borderColor}`,
+                     background: backColor
+                 }}
         >
-            <ReactSVG path="../assets/svg/success.svg"
+            <ReactSVG path={svgPath}
                       className="svg-wrapper"
                       svgStyle={{
                           width: 20,
                           height: 20
                       }}
             />
-            <span>{message || '忘记传递展示错误信息'}</span>
-        </section>,
-        showDom
+            <span>{messageString || '忘记传递展示错误信息'}</span>
+        </section>
     );
-};
+}
