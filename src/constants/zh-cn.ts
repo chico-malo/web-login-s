aee5bac67a91c47362661594990cb5093fb84bd0
yangyao <873241789@qq.com>
2018-08-10 18:21:59 +0800

    新增 后台status 状态码

---
+++ src/constants/zh-cn.ts
@@ -0,0 +1,13 @@
+/**
+ * Copyright: Copyright (C) 2018 sitb.software,All Rights Reserved
+ * author: yangyao(873241789@qq.com)
+ * date: 2018/8/10
+ */
+export const statusCode = {
+    '0000': '请求成功',
+    '1103': '用户已经存在',
+    '1104': '用户不存在',
+    '1203': '客户端已经存在',
+    '1204': '客户端不存在',
+    '3002': '验证码不正确或者超时'
+};
