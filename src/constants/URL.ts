913e99fdc55fc24efaaf0715da06373252a4ce34
yangyao <873241789@qq.com>
2018-08-10 15:27:57 +0800

    新增 url 静态常量

---
+++ src/constants/URL.ts
@@ -0,0 +1,9 @@
+/**
+ * Copyright: Copyright (C) 2018 sitb.software,All Rights Reserved
+ * author: yangyao(873241789@qq.com)
+ * date: 2018/8/10
+ */
+export default {
+    user: '/api/users',
+    check: '/api/register-check-value'
+}
