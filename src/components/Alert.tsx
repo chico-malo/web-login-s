998cf9e12831b782ca97f86b74ec302e06aafcf7
yangyao <873241789@qq.com>
2018-08-14 16:09:26 +0800

    优化弹框方法 分离操作方法 修改svg为 require

--- src/components/Alert.tsx
+++ src/components/Alert.tsx
@@ -6,9 +6,15 @@
 import '../styles/alert.scss';
 import * as ReactDOM from "react-dom";
 import * as React from "react";
-import ReactSVG from 'react-svg';
 import { statusCode } from '../constants/zh-cn';
 
+// 定时器变量
+let timer;
+
+/**
+ * 弹框组件
+ * @param {string | object} message
+ */
 export default function alert(message: string | object) {
     let body = document.body;
     let alert: any = document.getElementById('alert');
@@ -17,7 +23,7 @@ export default function alert(message: string | object) {
     if (alert) {
         return;
     }
-    // 设置基本属性
+    // 设置基本样式
     showDom.id = 'alert';
     showDom.style.position = 'absolute';
     showDom.style.top = '-130px';
@@ -29,34 +35,56 @@ export default function alert(message: string | object) {
     showDom.style.height = '100px';
     showDom.style.zIndex = 1;
     body.appendChild(showDom);
-    // 自我删除的方法
-    let close = () => {
-        ReactDOM.unmountComponentAtNode(showDom);
-        body.removeChild(showDom);
-    };
-    // 默认三秒之后关闭弹框
-    setTimeout(() => {
-        close();
-    }, 2000);
+    // 默认两秒之后关闭弹框
+    // timer = setTimeout(() => {
+    //     removeMessage()
+    // }, 2000);
     ReactDOM.render(
-        renderMessage(close, message),
+        renderMessage(removeMessage, message),
         showDom
     );
 };
 
+/**
+ * 删除方法,只要删除就清楚掉定时器，防止下次激活马上关闭弹框
+ */
+function removeMessage() {
+    let body = document.body;
+    let alert: any = document.getElementById('alert');
+    // 判断有弹框再清除
+    if (alert) {
+        ReactDOM.unmountComponentAtNode(alert);
+        body.removeChild(alert);
+        clearTimeout(timer);
+    }
+}
+
+/**
+ * 渲染 弹框
+ * @param close 关闭弹框函数
+ * @param message 弹框消息
+ * @returns {any}
+ */
 function renderMessage(close, message) {
     // 默认错误
-    let svgPath = '../assets/svg/wran.svg';
+    let svgPath = require('../assets/svg/wran.svg');
     let messageString = message;
-    let borderColor = '(255,229,143,1)';
+    let borderColor = 'rgba(255,229,143,1)';
     let backColor = 'rgba(255,251,230,1)';
     // 如果是对象
     if (message instanceof Object) {
         const {status, success} = message;
         messageString = statusCode[status];
-        svgPath = success && '../assets/svg/success.svg' || '../assets/svg/error.svg';
-        borderColor = success && 'rgba(183,235,143,1)' || 'rgba(255,163,158,1)';
-        backColor = success && 'rgba(246,255,237,1)' || 'rgba(246,255,237,1)';
+        // 失败
+        svgPath = require('../assets/svg/error.svg');
+        borderColor = 'rgba(255,163,158,1)';
+        backColor = 'rgba(246,255,237,1)';
+        // 成功
+        if (success) {
+            svgPath = require('../assets/svg/success.svg');
+            borderColor = 'rgba(183,235,143,1)';
+            backColor = 'rgba(246,255,237,1)';
+        }
     }
     return (
         <section className="alert"
@@ -66,12 +94,8 @@ function renderMessage(close, message) {
                      background: backColor
                  }}
         >
-            <ReactSVG path={svgPath}
-                      className="svg-wrapper"
-                      svgStyle={{
-                          width: 20,
-                          height: 20
-                      }}
+            <img className="svg"
+                 src={svgPath}
             />
             <span>{messageString || '忘记传递展示错误信息'}</span>
         </section>
