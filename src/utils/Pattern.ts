e01b6c5892334c33b5da3840c7d19a5c8d62d94e
yangyao <873241789@qq.com>
2018-08-14 18:31:04 +0800

    新增 正则表达式

---
+++ src/utils/Pattern.ts
@@ -0,0 +1,15 @@
+/**
+ * Copyright: Copyright (C) 2018 sitb.software,All Rights Reserved
+ * author: yangyao(873241789@qq.com)
+ * date: 2018/8/13
+ */
+export const pattern = {
+    /**
+     * 邮箱
+     */
+    email: /^[a-z0-9]+([._\\-]*[a-z0-9])*@([a-z0-9]+[-a-z0-9]*[a-z0-9]+.){1,63}[a-z0-9]+$/,
+    /**
+     * 手机号
+     */
+    phone: /^[1][3,4,5,7,8][0-9]{9}$/
+};
