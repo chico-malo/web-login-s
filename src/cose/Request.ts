62f459f3777b1def70a2de46d670d51dd7c3c5be
yangyao <873241789@qq.com>
2018-08-20 11:53:48 +0800

    修改 ajax请求

--- src/cose/Request.ts
+++ src/cose/Request.ts
@@ -3,12 +3,22 @@
  * author: yangyao(873241789@qq.com)
  * date: 2018/8/10
  */
-export function Request({url, method = 'GET', ...other}) {
+
+/**
+ * ajax请求
+ * @param {any} url 请求地址
+ * @param {any} method 请求方式
+ * @param {any} headers 自定义header
+ * @param {any} other 
+ * @returns {any}
+ * @constructor
+ */
+export function Request({url, method = 'GET', headers = {}, ...other}) {
     return fetch(url, {
         method: method,
         headers: {
-            'Accept': 'application/json',
-            'Content-Type': 'application/json'
+            'Content-Type': 'application/json',
+            ...headers
         },
         ...other
     }).then(res => res.json());
